c27bb679359637fb58270f292d330c22affe166c
inazll <ina.zell@stud.hs-mannheim.de>
2024-11-17 13:24:09 +0100

    layout with HeatMap and TabBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_heatmap_calendar/flutter_heatmap_calendar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,7 +31,14 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  late final TabController _tabController =
+      TabController(length: 2, vsync: this);
+  static const List<Tab> myTabs = <Tab>[
+    Tab(text: 'LEFT'),
+    Tab(text: 'RIGHT'),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,18 +54,59 @@ class _MyHomePageState extends State<MyHomePage> {
               },
             );
           },
-      
         ),
         actions: [
-      IconButton(
-        icon: Image.asset('assets/images/logo.png'),
-        onPressed: (){}
-      ),
-    ],
+          IconButton(
+              icon: Image.asset('assets/images/logo.png'), onPressed: () {}),
+        ],
       ),
       drawer: Drawer(child: ListView() // Populate the Drawer in the last step.
           ),
-      body: Center(),
+      body: Align(
+        alignment: Alignment.topCenter,
+        child: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+            return Column(
+              children: [
+                const Expanded(
+                  child: HeatMap(
+                    scrollable: true,
+                    // startDate: DateTime(DateTime.now().year, DateTime.now().month, 1),
+                    // endDate: DateTime(DateTime.now().year, DateTime.now().month+3),
+                    colorsets: {
+                      1: Colors.red,
+                      3: Colors.orange,
+                      5: Colors.yellow,
+                      7: Colors.green,
+                      9: Colors.blue,
+                      11: Colors.indigo,
+                      13: Colors.purple,
+                    },
+                  ),
+                ),
+                TabBar(
+                  controller: _tabController,
+                  tabs: myTabs,
+                ),
+                Expanded(
+                  child: TabBarView(
+                    controller: _tabController,
+                    children: myTabs.map((Tab tab) {
+                      final String label = tab.text!.toLowerCase();
+                      return Center(
+                        child: Text(
+                          'This is the $label tab',
+                          style: const TextStyle(fontSize: 36),
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                )
+              ],
+            );
+          },
+        ),
+      ),
     );
   }
 }
