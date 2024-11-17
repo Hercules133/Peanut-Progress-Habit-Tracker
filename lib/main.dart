1fdf4173820ce0bd691b5b2f507fb5aa198de635
inazll <ina.zell@stud.hs-mannheim.de>
2024-11-17 11:50:50 +0100

    edit AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Guten Morgen!'),
     );
   }
 }
@@ -37,7 +37,26 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+        leading: Builder(
+          builder: (context) {
+            return IconButton(
+              icon: const Icon(Icons.menu),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+            );
+          },
+      
+        ),
+        actions: [
+      IconButton(
+        icon: Image.asset('assets/images/logo.png'),
+        onPressed: (){}
       ),
+    ],
+      ),
+      drawer: Drawer(child: ListView() // Populate the Drawer in the last step.
+          ),
       body: Center(),
     );
   }
