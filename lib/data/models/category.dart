714579af1ff54edb8a6ce4bd2062e5984cbdd1e2
Oliver Klag <3003075@stud.hs-mannheim.de>
2024-11-20 17:05:15 +0100

    Habit class modified, category class

--- lib/data/models/category.dart
+++ lib/data/models/category.dart
@@ -1,1 +1,9 @@
+class Category {
+  String _name;
+  String _color; // (Hex code?)
+  // Optional, a description (?)
 
+  Category({required String name, required String color})
+      : _color = color,
+        _name = name;
+}
--- lib/data/models/habit.dart
+++ lib/data/models/habit.dart
@@ -1,28 +1,53 @@
-// ignore_for_file: unnecessary_getters_setters
+import 'dart:math';
+
+import 'package:streaks/data/models/category.dart';
+
+// habbit id ✅
+// category -> enum, seperate class ✅
+// time -> days and time ✅
+// iscompleted (days?) -> 2d array (?)
+// [[Mo, false], [Tu, false], [We, false], [Th, false], [Fr, false], [Sa, false], [Su, false]]
 
 class Habit {
+  // Used Id-s
+  static final Set<int> _usedIds = {};
+
   String _title;
   String _description;
-  List<DateTime> _time;
+  List<String> _days; // [Mo, Tu, We, Th, Fr, Sa, Su]
+  String _time;
   bool _isCompleted;
-  String _category;
-
-  // habbit id
-  // category -> enum, seperate class
-  // time -> days and time
-  // iscompleted (days?)
+  Category _category;
+  late int _id;
 
   Habit({
     required String title,
     required String description,
-    required List<DateTime> time,
+    required List<String> days,
+    required String time,
     bool isCompleted = false,
-    required String category,
+    required Category category,
   })  : _title = title,
         _description = description,
+        _days = days,
         _time = time,
         _isCompleted = isCompleted,
-        _category = category;
+        _category = category {
+    _id = generateId();
+  }
+
+  // Generate random number
+  static int generateId() {
+    final random = Random();
+    int newId;
+
+    do {
+      newId = random.nextInt(10000000);
+    } while (_usedIds.contains(newId));
+
+    _usedIds.add(newId);
+    return newId;
+  }
 
   // Getter for title
   String get title => _title;
@@ -41,20 +66,38 @@ class Habit {
   // Setter for description
   set description(String value) {
     if (value.length > 150) {
-      throw ArgumentError("Description cannot be longer then 150 characters!");
+      throw ArgumentError("Description cannot be longer than 150 characters!");
     }
     _description = value;
   }
 
+  // Getter for days
+  List<String> get days => _days;
+
+  // Setter for days
+  set days(List<String> value) {
+    if (value.isEmpty) {
+      throw ArgumentError("You have to choose the days!");
+    }
+    _days = value;
+  }
+
   // Getter for time
-  List<DateTime> get time => _time;
+  String get time => _time;
 
   // Setter for time
-  set time(List<DateTime> value) {
-    if (value.isEmpty) {
-      throw ArgumentError("You have to choose a time!");
+  set time(String value) {
+    if (!_isValidTimeFormat(value)) {
+      throw ArgumentError(
+          "Time must be in the format HH:mm, where HH is 00-23 and mm is 00-59.");
     }
     _time = value;
+  }
+
+  // Time format checker
+  bool _isValidTimeFormat(String time) {
+    final timeRegex = RegExp(r'^(?:[01]\d|2[0-3]):[0-5]\d$');
+    return timeRegex.hasMatch(time);
   }
 
   // Getter for isCompleted
@@ -66,12 +109,12 @@ class Habit {
   }
 
   // Getter for category
-  String get category => _category;
+  Category get category => _category;
 
   // Setter for category
-  set category(String value) {
-    if (value.isEmpty) {
-      throw ArgumentError("YOu have to choose a category!");
+  set category(Category value) {
+    if (value == null) {
+      throw ArgumentError("You have to choose a category!");
     }
     _category = value;
   }
