db3e3a8ff840011cf2e0e46c032e765ca94344b9
fezaanrehman <fezaanxali@icloud.com>
2024-11-20 15:52:18 +0100

    dev-fezaan first commit

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const MyHomePage(title: 'Guten Morgen!'),
+        home: const MyHomePage(title: 'Guten Morgen! -Fezaan Branch'),
       ),
     );
   }
