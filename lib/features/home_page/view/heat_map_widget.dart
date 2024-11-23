01b358e884d69edf708e918e1fe912720ac74ccc
inazll <ina.zell@stud.hs-mannheim.de>
2024-11-23 20:04:38 +0100

    edit heatMapWidth to ScreenWidth

--- lib/features/home_page/view/heat_map_widget.dart
+++ lib/features/home_page/view/heat_map_widget.dart
@@ -1,26 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_heatmap_calendar/flutter_heatmap_calendar.dart';
 
-class MyHeatMap extends StatelessWidget{  
+class MyHeatMap extends StatelessWidget {
   const MyHeatMap({super.key});
-
 
   @override
   Widget build(BuildContext context) {
-    return const HeatMap(
-                    scrollable: true,
-                    // startDate: DateTime(DateTime.now().year, DateTime.now().month, 1),
-                    // endDate: DateTime(DateTime.now().year, DateTime.now().month+3),
-                    colorsets: {
-                      1: Colors.red,
-                      3: Colors.orange,
-                      5: Colors.yellow,
-                      7: Colors.green,
-                      9: Colors.blue,
-                      11: Colors.indigo,
-                      13: Colors.purple,
-                    },
-                  );
+    final screenWidth = MediaQuery.of(context).size.width;
+    final monthsToShow = (screenWidth / 90).floor();
+    return Container(
+        
+        margin: const EdgeInsets.all(6),
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: const Color(0xFFB8936D),),
+        child: HeatMap(
+          textColor: const Color(0xFFC5CACD),
+          colorMode: ColorMode.color,
+          colorTipHelper: const [Text('0% ',style: TextStyle(color: const Color(0xFFC5CACD), )), Text(' 100% ', style: TextStyle(color: const Color(0xFFC5CACD),),) ],
+          scrollable: true,
+          size: 15,
+          defaultColor: const Color(0xFFDBDBDB),
+          startDate: DateTime(DateTime.now().year,
+              DateTime.now().month - monthsToShow, DateTime.now().day),
+          endDate: DateTime(DateTime.now().year, DateTime.now().month,
+              DateTime.now().day + 7),
+          datasets: m,
+          colorTipCount: 4,
+          colorsets: const {
+            0: Color(0xFFD3B09C),
+            1: Color(0xFFD6916B),
+            2: Color(0xFFFB7F3C),
+            3: Color(0xFFFF5A00),
+          },
+        ));
   }
-
 }
+
+Map<DateTime, int> m = {
+  DateTime(2024, 11, 18): 1,
+  DateTime(2024, 11, 20): 2,
+  DateTime(2024, 11, 23): 3
+};
