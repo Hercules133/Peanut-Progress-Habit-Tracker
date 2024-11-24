8b2c25ddbe1ff24016b26f98c3b85b5e11b42c53
Oliver Klag <3003075@stud.hs-mannheim.de>
2024-11-24 16:47:37 +0100

    Detail pop-up first version

--- lib/features/home_page/view/home_page_screen.dart
+++ lib/features/home_page/view/home_page_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:streaks/core/utils/hexcolor.dart';
+import 'package:streaks/data/models/habit.dart';
+import 'package:streaks/data/models/category.dart';
 
 import 'app_bar_widget.dart';
 import 'drawer_menu_widget.dart';
@@ -6,7 +9,15 @@ import 'heat_map_widget.dart';
 import 'tab_bar_widget.dart';
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+  MyHomePage({super.key});
+
+  Habit test_habit = Habit(
+      title: "Lunch",
+      description: "Eat lunch :)",
+      days: [0, 1, 4],
+      time: TimeOfDay(hour: 12, minute: 30),
+      habit_category: Habitcategory(
+          name: 'Eating', color: '#ec664a', icon: Icon(Icons.adobe)));
 
   static const List<String> myTabs = <String>[
     'Category 1',
@@ -32,14 +43,39 @@ class MyHomePage extends StatelessWidget {
         drawer: const MyDrawerMenu(),
         body: Column(
           children: [
-          const  MyHeatMap(),
+            const MyHeatMap(),
             const MyTabBar(tabs: myTabs),
             Expanded(
               child: TabBarView(
                 children: List.generate(
                   myTabs.length,
                   (index) => Center(
-                    child: Text(myTabs[index]),
+                    child: ListView.builder(
+                      itemCount: 1,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: EdgeInsets.all(15),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.amber,
+                            ),
+                            child: Column(
+                              children: [
+                                ListTile(
+                                  title: Text(test_habit.title),
+                                  trailing: IconButton(
+                                      onPressed: () {
+                                        showDetailsDialog(context, test_habit);
+                                      },
+                                      icon: Icon(Icons.info)),
+                                )
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
                   ),
                 ),
               ),
@@ -62,6 +98,73 @@ class MyHomePage extends StatelessWidget {
           child: const Icon(Icons.add),
         ),
       ),
+    );
+  }
+
+  Future<void> showDetailsDialog(BuildContext context, Habit habit) {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: ListTile(
+              title: Text(
+                habit.title,
+                style: TextStyle(fontSize: 25),
+              ),
+              leading: IconButton(
+                  onPressed: () {},
+                  icon: Image.asset('assets/images/Erdnuss.png')),
+              trailing: Row(mainAxisSize: MainAxisSize.min, children: [
+                Text(
+                  '${test_habit.streak}',
+                  style: TextStyle(fontSize: 12),
+                ),
+                IconButton(
+                  icon: Image.asset('assets/images/logo.png'),
+                  onPressed: () {},
+                ),
+              ])),
+          content: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                MyHeatMap(),
+                Text(
+                  habit.description,
+                  style: TextStyle(fontSize: 20),
+                ),
+                SizedBox(height: 20),
+              ],
+            ),
+          ),
+          actions: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('${test_habit.time.hour}:${test_habit.time.minute}'),
+                DecoratedBox(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: HexColor(test_habit.habit_category.color),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 8.0, vertical: 4.0), // Mehr Platz
+                    child: Text(test_habit.habit_category.name),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context)
+                        .pop(); // this has to take us to the edit page later
+                  },
+                  child: Icon(Icons.edit),
+                )
+              ],
+            ),
+          ],
+        );
+      },
     );
   }
 }
