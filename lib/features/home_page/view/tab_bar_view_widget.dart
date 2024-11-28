a2821e9fa8d23fb8abd858797291a1dec4a53732
Melinda Kienle <melinda.kienle@t-online.de>
2024-11-28 19:40:41 +0100

    refactor: format tab_bar_view_widget, add possible sources for charts in statistics

--- lib/features/home_page/view/tab_bar_view_widget.dart
+++ lib/features/home_page/view/tab_bar_view_widget.dart
@@ -24,12 +24,13 @@ class MyTabBarView extends StatelessWidget {
     Color.fromARGB(255, 199, 116, 57),
   ];
 
-   static Habit test_habit = Habit(
+  static Habit test_habit = Habit(
       title: "Lunch",
       description: "Cook and eat lunch",
       days: [0, 1, 4],
       time: const TimeOfDay(hour: 12, minute: 30),
-      category: Category(name: 'Eating', color: '#ec664a', icon: const Icon(Icons.adobe)));
+      category: Category(
+          name: 'Eating', color: '#ec664a', icon: const Icon(Icons.adobe)));
 
   @override
   Widget build(BuildContext context) {
@@ -37,51 +38,54 @@ class MyTabBarView extends StatelessWidget {
       children: List.generate(
         tabs.length,
         (tab) => ListView.separated(
-            physics: const BouncingScrollPhysics(),
-            shrinkWrap: true,
-            itemCount: 5,
-            padding: const EdgeInsets.all(8.0),
-            itemBuilder: (BuildContext context, int idx) {
-              return ListTile(
-                tileColor: categoryColor[tab],
-                textColor: Colors.white,
-                contentPadding: const EdgeInsets.only(
-                    left: 10.0, top: 2.0, bottom: 2.0, right: 10.0),
-                minVerticalPadding: 10.0,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10.0)),
-                leading: IconButton(
-                  icon: Image.asset('assets/images/Erdnuss.png'),
-                  onPressed: () => {},
-                ),
-                title: Text('Habit $idx of ${tabs[tab]}'),
-                trailing: Wrap(
-                  direction: Axis.vertical,
-                  children: [
-                    Row(
-                      children: [
-                        Text(
-                          '20',
-                          style: const TextStyle(color: Colors.white),
-                        ),
-                        Container(
-                          height: 30,
-                          child: Image.asset('assets/images/logo.png'),
-                        ),
-                      ],
-                    ),
-                    Text(
-                      '18:00',
-                      style: TextStyle(color: Colors.white),
-                    )
-                  ],
-                ),
-                onTap: () {showDetailsDialog(context, test_habit);},
-              );
-            },
-            separatorBuilder: (context, index) => const SizedBox(
-                  height: 5,
-                )),
+          physics: const BouncingScrollPhysics(),
+          shrinkWrap: true,
+          itemCount: 5,
+          padding: const EdgeInsets.all(8.0),
+          itemBuilder: (BuildContext context, int idx) {
+            return ListTile(
+              tileColor: categoryColor[tab],
+              textColor: Colors.white,
+              contentPadding: const EdgeInsets.only(
+                  left: 10.0, top: 2.0, bottom: 2.0, right: 10.0),
+              minVerticalPadding: 10.0,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0)),
+              leading: IconButton(
+                icon: Image.asset('assets/images/Erdnuss.png'),
+                onPressed: () => {},
+              ),
+              title: Text('Habit $idx of ${tabs[tab]}'),
+              trailing: Wrap(
+                direction: Axis.vertical,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        '20',
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                      Container(
+                        height: 30,
+                        child: Image.asset('assets/images/logo.png'),
+                      ),
+                    ],
+                  ),
+                  Text(
+                    '18:00',
+                    style: TextStyle(color: Colors.white),
+                  )
+                ],
+              ),
+              onTap: () {
+                showDetailsDialog(context, test_habit);
+              },
+            );
+          },
+          separatorBuilder: (context, index) => const SizedBox(
+            height: 5,
+          ),
+        ),
       ),
     );
   }
--- lib/features/statistics_page/view/statistics_screen.dart
+++ lib/features/statistics_page/view/statistics_screen.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:streaks/core/widgets/app_bar_widget.dart';
+import 'package:streaks/core/widgets/drawer_menu_widget.dart';
+import 'package:streaks/features/home_page/view/heat_map_widget.dart';
 
 class StatisticsPage extends StatelessWidget {
   const StatisticsPage({super.key});
 
-   @override
+  @override
   Widget build(BuildContext context) {
-    return Row(children: [Text('Statistics')],);
-    }
+    return Scaffold(
+      appBar: MyAppBar(
+        appBar: AppBar(),
+        appBarTitle: 'Statistics',
+      ),
+      drawer: const MyDrawerMenu(),
+      body: Column(
+        children: [
+          const MyHeatMap(),
+          Text('Diagrams'),
+          // https://pub.dev/packages/d_chart
+          // https://pub.dev/packages/graphic
+          // https://pub.dev/packages/syncfusion_flutter_charts
+          // https://pub.dev/packages/fl_chart
+          // https://pub.dev/packages/interactive_chart
 
+        ],
+      ),
+    );
+  }
 }
