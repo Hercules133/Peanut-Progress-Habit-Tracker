2ec9bdb41a8d83800ed68bf994dbca4d83afb86e
3003075 <3003075@stud.hs-mannheim.de>
2024-12-21 13:01:05 +0100

    Settings page responsive design

--- lib/features/settings_page/view/settings_page.dart
+++ lib/features/settings_page/view/settings_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '/core/widgets/app_bar_widget.dart';
 import '/core/widgets/drawer_menu_widget.dart';
-
 import 'switch_state.dart';
 
 class SettingsPage extends StatelessWidget {
@@ -18,142 +17,268 @@ class SettingsPage extends StatelessWidget {
         appBarTitle: 'Settings',
       ),
       drawer: const MyDrawerMenu(),
-      body: Column(
-        children: [
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Icon(
-                Icons.person_outline,
-                size: 55.0,
-                color: Theme.of(context).colorScheme.onSurface,
-              ),
-            ),
-          ),
-          const Align(
-            alignment: Alignment.centerLeft,
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 10.0),
-              child: Text("Username"),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: TextField(
-              decoration: const InputDecoration(
-                hintText: "Type in your name",
-              ),
-              cursorColor: Theme.of(context).colorScheme.onSurface,
-            ),
-          ),
-          const Align(
-            alignment: Alignment.centerLeft,
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 10.0),
-              child: Text("Languages"),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 10.0, left: 10.0, right: 10),
-            child: SizedBox(
-              width: double.infinity,
-              child: DropdownMenuTheme(
-                data: DropdownMenuThemeData(
-                  menuStyle: MenuStyle(
-                    shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(
-                            10.0), // Set the border radius
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth > 800) {
+            return Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Icon(
+                        Icons.person_outline,
+                        size: 55.0,
+                        color: Theme.of(context).colorScheme.onSurface,
                       ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text("Username"),
+                              TextField(
+                                decoration: const InputDecoration(
+                                  hintText: "Type in your name",
+                                ),
+                                cursorColor:
+                                    Theme.of(context).colorScheme.onSurface,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text("Languages"),
+                              DropdownButton<String>(
+                                value: 'English',
+                                onChanged: (String? newValue) {},
+                                items: const <DropdownMenuItem<String>>[
+                                  DropdownMenuItem<String>(
+                                    value: 'English',
+                                    child: Text('English'),
+                                  ),
+                                  DropdownMenuItem<String>(
+                                    value: 'Deutsch',
+                                    child: Text('Deutsch'),
+                                  ),
+                                ],
+                                isExpanded: true,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text("Cloud"),
+                              TextField(
+                                decoration: const InputDecoration(
+                                  hintText: "none",
+                                ),
+                                cursorColor:
+                                    Theme.of(context).colorScheme.onSurface,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text("Theme Mode"),
+                              DropdownButton<String>(
+                                value:
+                                    _getInitialSelection(switchState.themeMode),
+                                onChanged: (String? newValue) {
+                                  if (newValue != null) {
+                                    switch (newValue) {
+                                      case 'dark':
+                                        switchState
+                                            .toggleThemeMode(ThemeMode.dark);
+                                        break;
+                                      case 'light':
+                                        switchState
+                                            .toggleThemeMode(ThemeMode.light);
+                                        break;
+                                      case 'system':
+                                        switchState
+                                            .toggleThemeMode(ThemeMode.system);
+                                        break;
+                                    }
+                                  }
+                                },
+                                items: const <DropdownMenuItem<String>>[
+                                  DropdownMenuItem<String>(
+                                    value: 'dark',
+                                    child: Text('Dark'),
+                                  ),
+                                  DropdownMenuItem<String>(
+                                    value: 'light',
+                                    child: Text('Light'),
+                                  ),
+                                  DropdownMenuItem<String>(
+                                    value: 'system',
+                                    child: Text('System'),
+                                  ),
+                                ],
+                                isExpanded: true,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            );
+          } else {
+            return Column(
+              children: [
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Icon(
+                      Icons.person_outline,
+                      size: 55.0,
+                      color: Theme.of(context).colorScheme.onSurface,
                     ),
                   ),
                 ),
-                child: DropdownMenu(
-                  inputDecorationTheme:
-                      const InputDecorationTheme(filled: true),
-                  initialSelection: 'English',
-                  //inputDecorationTheme: InputDecorationTheme(),
-                  trailingIcon: Icon(
-                    Icons.arrow_drop_down,
-                    color: Theme.of(context).colorScheme.onSurface,
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10.0),
+                    child: Text("Username"),
                   ),
-                  width: double.infinity,
-                  dropdownMenuEntries: const <DropdownMenuEntry<String>>[
-                    DropdownMenuEntry(value: 'English', label: 'English'),
-                    DropdownMenuEntry(value: 'Deutsch', label: 'Deutsch'),
-                  ],
                 ),
-              ),
-            ),
-          ),
-          const Align(
-            alignment: Alignment.centerLeft,
-            child: Padding(
-              padding: EdgeInsets.only(top: 10.0, left: 10.0),
-              child: Text("Cloud"),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: TextField(
-              decoration: const InputDecoration(
-                hintText: "none",
-              ),
-              cursorColor: Theme.of(context).colorScheme.onSurface,
-            ),
-          ),
-          const Align(
-            alignment: Alignment.centerLeft,
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 10.0),
-              child: Text("Theme Mode"),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 10.0, left: 10, right: 10),
-            child: SizedBox(
-              width: double.infinity,
-              child: DropdownMenuTheme(
-                data: DropdownMenuThemeData(
-                  menuStyle: MenuStyle(
-                    shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(
-                            10.0), // Set the border radius
-                      ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: TextField(
+                    decoration: const InputDecoration(
+                      hintText: "Type in your name",
+                    ),
+                    cursorColor: Theme.of(context).colorScheme.onSurface,
+                  ),
+                ),
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10.0),
+                    child: Text("Languages"),
+                  ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.only(top: 10.0, left: 10.0, right: 10),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: DropdownButton<String>(
+                      value: 'English',
+                      onChanged: (String? newValue) {},
+                      items: const <DropdownMenuItem<String>>[
+                        DropdownMenuItem<String>(
+                          value: 'English',
+                          child: Text('English'),
+                        ),
+                        DropdownMenuItem<String>(
+                          value: 'Deutsch',
+                          child: Text('Deutsch'),
+                        ),
+                      ],
+                      isExpanded: true,
                     ),
                   ),
                 ),
-                child: DropdownMenu(
-                  inputDecorationTheme:
-                      const InputDecorationTheme(filled: true),
-                  width: double.infinity,
-                  initialSelection: _getInitialSelection(switchState.themeMode),
-                  dropdownMenuEntries: const <DropdownMenuEntry<String>>[
-                    DropdownMenuEntry(value: 'dark', label: 'Dark'),
-                    DropdownMenuEntry(value: 'light', label: 'Light'),
-                    DropdownMenuEntry(value: 'system', label: 'System')
-                  ],
-                  onSelected: (String? newValue) {
-                    if (newValue != null) {
-                      switch (newValue) {
-                        case 'dark':
-                          switchState.toggleThemeMode(ThemeMode.dark);
-                          break;
-                        case 'light':
-                          switchState.toggleThemeMode(ThemeMode.light);
-                          break;
-                        case 'system':
-                          switchState.toggleThemeMode(ThemeMode.system);
-                          break;
-                      }
-                    }
-                  },
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 10.0, left: 10.0),
+                    child: Text("Cloud"),
+                  ),
                 ),
-              ),
-            ),
-          ),
-        ],
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: TextField(
+                    decoration: const InputDecoration(
+                      hintText: "none",
+                    ),
+                    cursorColor: Theme.of(context).colorScheme.onSurface,
+                  ),
+                ),
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10.0),
+                    child: Text("Theme Mode"),
+                  ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.only(top: 10.0, left: 10, right: 10),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: DropdownButton<String>(
+                      value: _getInitialSelection(switchState.themeMode),
+                      onChanged: (String? newValue) {
+                        if (newValue != null) {
+                          switch (newValue) {
+                            case 'dark':
+                              switchState.toggleThemeMode(ThemeMode.dark);
+                              break;
+                            case 'light':
+                              switchState.toggleThemeMode(ThemeMode.light);
+                              break;
+                            case 'system':
+                              switchState.toggleThemeMode(ThemeMode.system);
+                              break;
+                          }
+                        }
+                      },
+                      items: const <DropdownMenuItem<String>>[
+                        DropdownMenuItem<String>(
+                          value: 'dark',
+                          child: Text('Dark'),
+                        ),
+                        DropdownMenuItem<String>(
+                          value: 'light',
+                          child: Text('Light'),
+                        ),
+                        DropdownMenuItem<String>(
+                          value: 'system',
+                          child: Text('System'),
+                        ),
+                      ],
+                      isExpanded: true,
+                    ),
+                  ),
+                ),
+              ],
+            );
+          }
+        },
       ),
     );
   }
