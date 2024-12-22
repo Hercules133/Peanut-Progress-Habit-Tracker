7b6a89549c1a5e92c9a8a6794624154f01d522d0
inazll <ina.zell@stud.hs-mannheim.de>
2024-12-22 10:26:01 +0100

    add Test CoreAppBar

---
+++ test/core_app_bar_widget_test.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:streaks/core/widgets/app_bar_widget.dart';
+
+void main() {
+  testWidgets(
+    'AppBar has all needed icons and text',
+    (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            appBar: MyAppBar(
+              appBar: AppBar(),
+              appBarTitle: 'Test AppBar',
+            ),
+          ),
+        ),
+      );
+      expect(find.text('Test AppBar'), findsOneWidget);
+
+      final logoFinder = find.byType(Image);
+      expect(logoFinder, findsOneWidget);
+
+      final Image logoImage = tester.widget<Image>(logoFinder);
+      expect(logoImage.image, isA<AssetImage>());
+
+      final AssetImage assetImage = logoImage.image as AssetImage;
+      expect(assetImage.assetName, 'assets/images/logo.png');
+    },
+  );
+
+  testWidgets(
+    'AppBar has IconButtons',
+    (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            appBar: MyAppBar(
+              appBar: AppBar(),
+              appBarTitle: 'Test AppBar',
+            ),
+          ),
+        ),
+      );
+      expect(find.byType(IconButton), findsExactly(2));
+    },
+  );
+
+  testWidgets(
+    'actions on IconButtons are correct',
+    (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            appBar: MyAppBar(
+              appBar: AppBar(),
+              appBarTitle: 'Test AppBar',
+            ),
+            drawer: const Drawer(),
+          ),
+        ),
+      );
+      expect(find.byType(Drawer), findsNothing);
+      await tester.tap(find.byIcon(Icons.menu));
+      await tester.pump();
+      expect(find.byType(Drawer), findsOneWidget);
+
+      final imageButton = find.widgetWithImage(
+          IconButton, const AssetImage('assets/images/logo.png'));
+
+      final IconButton widget = tester.widget<IconButton>(imageButton);
+      expect(widget.onPressed, isNull);
+    },
+  );
+}
