f9f0978d80557b9017a3afe0dd669d84ab28fadc
Oliver Klag <3003075@stud.hs-mannheim.de>
2025-01-04 13:16:02 +0100

    Statistic page without backend

--- lib/features/statistics_page/view/statistics_screen.dart
+++ lib/features/statistics_page/view/statistics_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:peanutprogress/features/statistics_page/view/bar_chart_widget.dart';
+import 'package:peanutprogress/features/statistics_page/view/line_chart_widget.dart';
+import 'package:peanutprogress/features/statistics_page/view/pie_chart_widget.dart';
 import '/core/widgets/app_bar_widget.dart';
 import '/core/widgets/drawer_menu_widget.dart';
 import '/features/home_page/view/heat_map_widget.dart';
@@ -14,16 +17,41 @@ class StatisticsPage extends StatelessWidget {
         appBarTitle: 'Statistics',
       ),
       drawer: const MyDrawerMenu(),
-      body: const Column(
-        children: [
-          MyHeatMap(),
-          Text('Diagrams'),
-          // https://pub.dev/packages/d_chart
-          // https://pub.dev/packages/graphic
-          // https://pub.dev/packages/syncfusion_flutter_charts
-          // https://pub.dev/packages/fl_chart
-          // https://pub.dev/packages/interactive_chart
-        ],
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth > 600) {
+            return SingleChildScrollView(
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Expanded(child: const MyHeatMap()),
+                      Expanded(child: PieChartWidget()),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Expanded(child: BarChartWidget()),
+                      Expanded(child: LineChartWidget()),
+                    ],
+                  ),
+                ],
+              ),
+            );
+          } else {
+            return SingleChildScrollView(
+              child: Column(
+                children: [
+                  const MyHeatMap(),
+                  const Text('Diagrams'),
+                  PieChartWidget(),
+                  BarChartWidget(),
+                  LineChartWidget(),
+                ],
+              ),
+            );
+          }
+        },
       ),
     );
   }
