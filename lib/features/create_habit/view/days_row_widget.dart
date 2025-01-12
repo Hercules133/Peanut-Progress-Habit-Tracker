5658359428a73c22539ec08475b4017e6635a125
3003075 <3003075@stud.hs-mannheim.de>
2025-01-12 10:05:36 +0100

    Fixed days and Time text on creade/edit habit page

--- lib/features/create_habit/view/days_row_widget.dart
+++ lib/features/create_habit/view/days_row_widget.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import '/features/create_habit/view/day_button_widget.dart';
 import '/features/create_habit/view/time_button_widget.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class DaysRowWidget extends StatelessWidget {
   const DaysRowWidget({super.key});
-
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
@@ -13,7 +11,7 @@ class DaysRowWidget extends StatelessWidget {
         if (constraints.maxWidth > 600) {
           return Row(
             children: [
-              Text(AppLocalizations.of(context)!.daysRowWidgetDays),
+              Text("Days "),
               Expanded(
                 child: SizedBox(
                   height: 30,
@@ -32,28 +30,42 @@ class DaysRowWidget extends StatelessWidget {
                 ),
               ),
               const SizedBox(width: 20),
-              Text(AppLocalizations.of(context)!.daysRowWidgetReminder),
+              Text("Time"),
               const SizedBox(width: 10),
               TimeButtonWidget(),
             ],
           );
         } else {
-          return SizedBox(
-            height: 30,
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              children: [
-                DayButtonWidget(day: "Mo"),
-                DayButtonWidget(day: "Tu"),
-                DayButtonWidget(day: "We"),
-                DayButtonWidget(day: "Th"),
-                DayButtonWidget(day: "Fr"),
-                DayButtonWidget(day: "Sa"),
-                DayButtonWidget(day: "Su"),
-                const SizedBox(width: 20),
-                TimeButtonWidget(),
-              ],
-            ),
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Days"),
+              const SizedBox(height: 10),
+              Row(
+                children: [
+                  Expanded(
+                    child: SizedBox(
+                      height: 30,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: [
+                          DayButtonWidget(day: "Mo"),
+                          DayButtonWidget(day: "Tu"),
+                          DayButtonWidget(day: "We"),
+                          DayButtonWidget(day: "Th"),
+                          DayButtonWidget(day: "Fr"),
+                          DayButtonWidget(day: "Sa"),
+                          DayButtonWidget(day: "Su"),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Text("Time"),
+              const SizedBox(height: 10),
+              TimeButtonWidget(),
+            ],
           );
         }
       },
